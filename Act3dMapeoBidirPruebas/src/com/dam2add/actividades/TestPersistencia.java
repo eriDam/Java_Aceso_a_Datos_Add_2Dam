2cd56ab350bd6230ee86e85bfc922fd998ab3266
eriDam <erika.c.caballero@gmail.com>
2014-12-14 23:06:59 +0100

    Changes Create in first order empresa, in second nomina...

--- Act3dMapeoBidirPruebas/src/com/dam2add/actividades/TestPersistencia.java
+++ Act3dMapeoBidirPruebas/src/com/dam2add/actividades/TestPersistencia.java
@@ -21,30 +21,38 @@ public class TestPersistencia {
 			EntityManager em = emf.createEntityManager();
 			Persona p=null;
 			
-			//Selecciono de la Bd la primera persona
-			em.getTransaction().begin();
+			
+			
 			//Creo Empresa
 			System.out.println("Creando Empresa...");
 			Empresa e = new Empresa("El corte");
-			Query q = em.createQuery("SELECT p FROM Persona p");
-			p = (Persona) q.getResultList().get(0);	
-			
-			Nomina nom= new Nomina(1000,p);
+			em.getTransaction().begin();
 			em.persist(e);
-			em.persist(nom);
+			em.persist(e);
 			em.getTransaction().commit();
 			System.out.println("***Empresa creada ok***");
 			e.print();
 			
+			
+			//Creo nomina
+		    Nomina nom= new Nomina(1000,p);
+		    em.getTransaction().begin();
+			em.persist(nom);
+			em.getTransaction().commit();
+			System.out.println("***Nomina creada ok***");
+			
 			//creo persona
 			System.out.println("Creando persona...");
-		    p=new Persona ("29204528w",32,"Eri", nom,e);	
+		    Persona p1=new Persona ("29204528w",32,"Eri", nom,e);	
 			em.getTransaction().begin();//se activa la transaccion mediante el método begin 
-			em.persist(p);//va a almacenar la informacion de p en la bd, el persist se utiliza para persistir nuevas entidades en la bd
+			em.persist(p1);//va a almacenar la informacion de p en la bd, el persist se utiliza para persistir nuevas entidades en la bd
 			em.getTransaction().commit();//y finaliza cuando hay un commit
 			System.out.println("***Persona creada***"); 
 			p.print();
-			 
+			
+			//Selecciono de la Bd la primera persona
+			Query q = em.createQuery("SELECT p FROM Persona p");
+			p = (Persona) q.getResultList().get(0);	
 			
 			
 		} catch (Exception ex) {
