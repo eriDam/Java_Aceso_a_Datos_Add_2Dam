32ee6bc985556b77050b0812bd9f38c48e029bef
eriDam <erika.c.caballero@gmail.com>
2015-01-30 20:42:24 +0100

    Add comments in example and generate javaDoc
    Signed-off-by: eriDam <erika.c.caballero@gmail.com>

--- Act5cExampleXQJ/src/Connect.java
+++ Act5cExampleXQJ/src/Connect.java
@@ -5,65 +5,88 @@ import javax.xml.xquery.XQExpression;
 import javax.xml.xquery.XQResultSequence;
 /**
  * Esta Aplicación representa un ejemplo de utilizacion de XQJ
+ *
+ * Importamos las 3 librerias que nos proporcina el sigiente API.
  * API :  http://xqj.net/javadoc/  
- * 
  * 
  * */
 public class Connect {
 
 	public static void main(String[] args) {
-		/**Instanciamos el objeto de XQconnection*/
+		/**1-Instanciamos el objeto de XQconnection*/
 		XQConnection conn = null;
 				
 		try {
-			// Abrimos la sesión
-			/**XQDataSource es una fábrica  de XQConnection objetos.
-			 * XQDataSource xqs = new XQDataSource BaseXXQDataSource();
-			 * Pero no lo instanciaremos así, la forma de proceder para poder conectarnos contra 
-			 * otros Sistemas gestores, utilizamos un string que nos haga referencia al 
-			 * deriver que vamos a utilizar y despues generar (casteando) un objeto XQDataSource 
+			// Abrimos la sesión en la base de datos
+			/**2-XQDataSource es una fábrica  de objetos XQConnection.
+			 * forma: XQDataSource xqs = new XQDataSource BaseXXQDataSource();
+			 * 
+			 * Pero no lo instanciaremos así, la forma de proceder PARA PODER CONECTARNOS contra 
+			 * OTROS Sistemas gestores de Bd, utilizamos un string que nos haga referencia al 
+			 * driver que vamos a utilizar.
+			 * 
+			 *Despues generar (casteando) un objeto XQDataSource 
 			 * haremos un  Class.forName de ese driver que queremos utilizar*/
 			XQDataSource xqs = (XQDataSource) Class.forName("net.xqj.basex.BaseXXQDataSource").newInstance();
 			
-			/**Realizamos un for para recorrer las propiedades del vector y que nos las muestre */
-			/**Si no sabemos las propiedades del sistema gestor de bd determinado, para ver como 
-			 * obtenerlas, con getSupportedPropertyNames, las obtenemos, es un vector de Strings que nos
-			 * devuelve las distintas propiedades que tiene*/
+			/**Realizamos un BUCLE for para recorrer las propiedades del vector y que nos las muestre */
+			
+			/**4- Si no sabemos las propiedades del sistema gestor de bd determinado, para ver como 
+			 * OBTENERLAS, con getSupportedPropertyNames, las obtenemos, es un vector de Strings que nos
+			 * devuelve las distintas propiedades que tiene el sistema gestor de DB*/
 			System.out.println("Obtengo las propiedades");
 			for (int i=0;i<xqs.getSupportedPropertyNames().length;i++)
 				System.out.println(xqs.getSupportedPropertyNames()[i]);
 			
-			/**Si quieremos indicar las propiedades, maquina, puerto...*/
+			/**Si queremos indicar las propiedades, maquina, puerto...*/
 			xqs.setProperty("serverName", "localhost");
 			xqs.setProperty("port", "1984");
 			xqs.setProperty("user", "admin");
 			xqs.setProperty("password", "admin");
-			/**Cuando ya tenemos un objeto de tipo XQDataSource, podemos establecer la conexión 
+			
+			/**3- Cuando ya tenemos un objeto de tipo XQDataSource, podemos establecer la conexión 
 			 * con una serie de parámetros o sin ellos con getConnection, hay 3 métodos en este 
 			 * caso utilizo el método sin ellos*/
 			conn = xqs.getConnection();
 
 			System.out.println("Connexión correcta con el SGBD BaseX");
 			
-			/** Creamos XQExpression:
-			 * */
+			/** Creamos XQExpression:  para la ejecución inmediata de sentencias XQuery
+			 *  Este objeto puede ser creado a partir de la XQConnection y la ejecución se puede 
+			 *  hacer usando el executeQuery() o executeCommand() método, que pasa en la expresión XQuery.
+			 *  
+			 *  Sirve para ejecutar sentencias de consulata: conjunto de resultados, y pueden procesar ordenes 
+			 *  de insercion, eliminacion y actualizacion.
+			 *  
+			 *  Creo el objeto xqe que lo obtengo de XQConnection (conn)mediante createExpression*/
 			XQExpression xqe = conn.createExpression();
 			// Preparamos la instrucción para BaseX
 			String cad;
 			cad = "for $c in doc('personas')/personas/persona return $c/nombre";
 
-			// Ejecutamos
-			System.out.println("Ejecutamos consulta: " + cad);
-			/** Las sentencias de consultas pueden devolver un conjunto de resultados, 
-			 * en caso de quere obtener resultados,*/
+			/**Ejecutamos:
+			 * 
+			 * Las sentencias de consultas XQExpression pueden devolver un CONJUNTO DE RESULTADOS, 
+			 * en caso de querer obtener resultados.
+			 * 
+			 * Esta interfaz XQResultSequence representa una secuencia de elementos obtenidos 
+			 * como resultado de expresiones XQuery evaluación.
+			 * La secuencia resultado está ligada a la XQconnection objeto en el que se evaluó la expresión.
+			 * */
+			System.out.println("Ejecutamos consulta: " + cad);	
+			/** Ejecutamos la consulta  xqe.executeQuery pasandole la cadena y 
+			 * obtenemos en el  XQResultSequence xqrs el resultado de esa ejecución*/
 			XQResultSequence xqrs = xqe.executeQuery(cad);
-			// Mostramos los resultados, uno a uno, convertidos a String
-			System.out.println("\nResultados:");
+			
+			/**Para poder mostrar los resultados, uno a uno, convertidos a String mediante un bucle While
+			 * utilizando el método next, vamos recorriendo y con xqrs.getItemAsString(null), va a mostrar
+			 * losdistintos nombres de las personas que hay.*/
+			System.out.println("\nLos resultados son: ");
 			while (xqrs.next())
 				System.out.println(xqrs.getItemAsString(null));
 		} catch (Exception e) {
 			e.printStackTrace();
-		} finally { // Cerramos la sesión
+		} finally { /** Cerramos la sesión*/
 			try {
 				if (conn != null)
 					conn.close();
